b1b7795478b670ea282bd832696f489dd4283f31
Yaprak Ayazoglu <info@frontconnect.nl>
2020-02-10 22:00:37 +0100

    Init
    - Add typescript
    - Create domains rather than flat folder structure
    - Create model files for Google Sheets and Google Sheet Rows
    - Make naming verbose

--- src/api/MoneyBird/moneybird.ts
+++ src/api/MoneyBird/moneybird.ts
@@ -1,3 +1,6 @@
+import { AuthConfig } from "../../auth/AuthConfig";
+import { InvoiceRow } from "../GoogleSheets/InvoiceRow.model";
+
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
@@ -7,25 +10,25 @@ let apiBaseUrl = '/api/v2/';
 
 const ax = axios.create({
   baseURL: 'https://moneybird.com',
-  validateStatus: status => status >= 200 && status < 500
+  validateStatus: (status: number) => status >= 200 && status < 500
 });
 
-const getAuthRequestToken = (authCode, cfg) => {
+const getAuthRequestToken = (authCode: string, authConfig: AuthConfig) => {
   return ax.post('/oauth/token', {
     redirect_uri: 'urn:ietf:wg:oauth:2.0:oob',
     grant_type: 'authorization_code',
-    client_id: cfg.client_id,
-    client_secret: cfg.client_secret,
-    code: authCode
+    client_id: authConfig.client_id,
+    client_secret: authConfig.client_secret,
+    code: authCode,
   });
 };
 
-const refreshAccessToken = async (refreshToken, cfg) => {
+const refreshAccessToken = async (refreshToken: string, authConfig: AuthConfig) => {
   const res = await ax.post('/oauth/token', {
     grant_type: 'refresh_token',
     refresh_token: refreshToken,
-    client_id: cfg.client_id,
-    client_secret: cfg.client_secret
+    client_id: authConfig.client_id,
+    client_secret: authConfig.client_secret
   });
   if (res.status !== 200) {
     throw new Error(`Error refreshing access token: ${res.status} ${JSON.stringify(res.data)}`);
@@ -34,7 +37,8 @@ const refreshAccessToken = async (refreshToken, cfg) => {
   return res.data.access_token;
 };
 
-const writeTokenFile = (token) => {
+// TODO: what is the type of token?
+const writeTokenFile = (token: any) => {
   fs.writeFileSync(TOKEN_FILE, JSON.stringify(token, null, 2));
 };
 
@@ -48,7 +52,7 @@ const getZeroVatRateId = async () => {
   if (res.status !== 200) {
     throw new Error(`Unable to get Moneybird tax rates; ${res.status} ${JSON.stringify(res.data)}`);
   }
-  const rate = res.data.find(rate => parseFloat(rate.percentage) === 0);
+  const rate = res.data.find((rate: string) => parseFloat(rate.percentage) === 0);
   return rate ? rate.id : false;
 };
 
@@ -58,15 +62,15 @@ const getZeroVatRateId = async () => {
  * @param {object} cfg
  * @returns {Promise<string>} invoiceId
  */
-const createSalesInvoice = async (rows, includeVat = true, cfg) => {
-  let mbRows = rows.map(row => row.toMoneybirdRow());
+const createSalesInvoice = async (rows: InvoiceRow[], includeVat = true, cfg: any) => {
+  let moneyBirdRows = rows.map((row: InvoiceRow) => InvoiceRow.toMoneyBirdRow(row));
 
   if (includeVat === false) {
     const rateId = await getZeroVatRateId();
     if (!rateId) {
       throw new Error('VAT to be excluded, but 0%-VAT rate ID could not be determined');
     }
-    mbRows.forEach(row => {
+    moneyBirdRows.forEach(row => {
       row.tax_rate_id = rateId;
     });
   }
@@ -74,7 +78,7 @@ const createSalesInvoice = async (rows, includeVat = true, cfg) => {
   const res = await ax.post(`${apiBaseUrl}/sales_invoices`, {
     sales_invoice: {
       contact_id: cfg.dummy_contact_id,
-      details_attributes: mbRows
+      details_attributes: moneyBirdRows
     }
   });
   if (res.status !== 201) {
@@ -83,22 +87,22 @@ const createSalesInvoice = async (rows, includeVat = true, cfg) => {
   return res.data.id;
 };
 
-module.exports = (mbcfg) => {
-  apiBaseUrl += mbcfg.administration_id;
+module.exports = (moneyBirdConfig: any) => {
+  apiBaseUrl += moneyBirdConfig.administration_id;
 
   return {
     init: async () => {
       if (fs.existsSync(TOKEN_FILE)) {
-        const accessToken = await refreshAccessToken(require(TOKEN_FILE).refresh_token, mbcfg);
-        ax.interceptors.request.use(config => {
+        const accessToken = await refreshAccessToken(require(TOKEN_FILE).refresh_token, moneyBirdConfig);
+        ax.interceptors.request.use((config: any) => {
           config.headers.authorization = `Bearer ${accessToken}`;
           return config;
         });
       }
     },
-    getAuthRequestToken: authCode => getAuthRequestToken(authCode, mbcfg),
+    getAuthRequestToken: (authCode: string) => getAuthRequestToken(authCode, moneyBirdConfig),
     writeTokenFile,
     getAllSalesInvoices,
-    createSalesInvoice: (rows, includeVat) => createSalesInvoice(rows, includeVat, mbcfg)
+    createSalesInvoice: (rows: any, includeVat: any) => createSalesInvoice(rows, includeVat, moneyBirdConfig)
   };
 };
